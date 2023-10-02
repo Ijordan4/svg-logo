5dee1c7e152704e93b0f1375fc461b781d8c3bf9
ijordan4 <isaiahjordan200@gmail.com>
2023-10-02 13:19:24 -0400

    updated files in question

--- index.js
+++ index.js
@@ -1,56 +1,50 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateLogoSvg = require('./lib/shapes');
 
-const promptUser = () => {
-  inquirer
-    .prompt([
+const generateLogo = require('./lib/shapes');
+
+const getUserInput = async () => {
+  try {
+    const userInput = await inquirer.prompt([
       {
-        name: 'text',
-        message: 'Please enter up to three characters for the logo text',
-        validate: (input) => {
-          if (input.length > 3) {
-            return 'Please only enter up to three characters.';
-          }
-          return true;
-        },
+        name: 'logoText',
+        message: 'Enter up to three characters for your logo text:',
+        validate: (input) => input.length <= 3 || 'Please enter up to three characters.',
       },
-    
       {
         name: 'textColor',
-        message: 'Please enter the color of your choosing for the logo text (or a hexadecimal number)',
+        message: 'Choose a color for the logo text (color name or hexadecimal code):',
         default: 'white',
       },
       {
         type: 'list',
         name: 'shape',
-        message: 'Please choose a shape for the logo:',
+        message: 'Select a shape for your logo:',
         choices: ['circle', 'triangle', 'square'],
       },
       {
         name: 'shapeColor',
-        message: 'Please enter the color for the desired shape of your choosing (keyword or hexadecimal):',
+        message: 'Enter the color for your chosen shape (color name or hexadecimal code):',
         default: 'black',
       },
-    ])
-    .then((answers) => {
-      const { text, textColor, shape, shapeColor } = answers;
-      const svgTemplate = generateLogoSvg(text, textColor, shape, shapeColor);
-      saveSvgToFile(svgTemplate);
-    })
-    .catch((error) => {
-      console.log('An error occurred:', error);
-    });
+    ]);
+
+    const { logoText, textColor, shape, shapeColor } = userInput;
+    const logoSvg = generateLogo(logoText, textColor, shape, shapeColor);
+    saveSvgToFile(logoSvg);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 };
 
 const saveSvgToFile = (svgTemplate) => {
   fs.writeFile('logo.svg', svgTemplate, (err) => {
     if (err) {
-      console.log('Error while saving SVG file', err);
+      console.error('Error while saving the SVG file:', err);
     } else {
-      console.log('Generated logo.svg');
-    };
+      console.log('Logo saved as logo.svg');
+    }
   });
 };
 
-promptUser();
+getUserInput();
